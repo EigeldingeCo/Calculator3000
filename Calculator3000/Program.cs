2ef7341fdd221e98801da0328a7eb65d2130d257
Corto Eigeldinger <corto.eigeldinger@rpn.ch>
2017-12-13 08:55:16 +0100

    ajout de la branche français

--- Calculator3000/Program.cs
+++ Calculator3000/Program.cs
@@ -14,6 +14,10 @@ namespace Calculator3000
             double dblNoteSaisieAt;
             double dblMoyenneAt;
 
+            double dblNoteFr = 0;
+            double dblNoteSaisieFr;
+            double dblMoyenneFr;
+
             Console.Write("Combien de notes d'atelier voulez-vous insérer? : ");
             string strnbNoteAt = Console.ReadLine();
             int iNbNoteAt = Convert.ToInt32(strnbNoteAt);
@@ -38,6 +42,28 @@ namespace Calculator3000
             Console.WriteLine("Votre moyenne est de {0}", dblMoyenneAt);
             Console.ReadLine();
 
+            Console.Write("Combien de notes de français voulez-vous insérer? : ");
+            string strnbNoteFr = Console.ReadLine();
+            int iNbNoteFr = Convert.ToInt32(strnbNoteFr);
+            int iNoteResFr = iNbNoteFr;
+
+            do
+            {
+                Console.Write("Entrez votre note : ");
+                string strSaisieFr = Console.ReadLine();
+                dblNoteSaisieFr = Convert.ToDouble(strSaisieFr);
+
+                dblNoteFr += dblNoteSaisieFr;
+
+                iNoteResFr -= 1;
+
+            }
+            while (iNoteResFr != 0);
+
+            dblMoyenneFr = dblNoteFr / iNbNoteFr;
+
+            Console.WriteLine("Votre moyenne est de {0}", dblMoyenneFr);
+            Console.ReadLine();
         }
     }
 }
