aeea283756a4adc3a591f6ffbb5714493ca145ae
Corto Eigeldinger <corto.eigeldinger@rpn.ch>
2017-12-13 08:52:37 +0100

    ajout de la branche atelier

---
+++ Calculator3000/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator3000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dblNoteAt = 0;
+            double dblNoteSaisieAt;
+            double dblMoyenneAt;
+
+            Console.Write("Combien de notes d'atelier voulez-vous insérer? : ");
+            string strnbNoteAt = Console.ReadLine();
+            int iNbNoteAt = Convert.ToInt32(strnbNoteAt);
+            int iNoteResAt = iNbNoteAt;
+
+            do
+            {
+                Console.Write("Entrez votre note : ");
+                string strSaisieAt = Console.ReadLine();
+                dblNoteSaisieAt = Convert.ToDouble(strSaisieAt);
+
+                dblNoteAt += dblNoteSaisieAt;
+
+                iNoteResAt -= 1;
+
+            }
+            while (iNoteResAt != 0);
+
+            dblMoyenneAt = dblNoteAt / iNbNoteAt;
+
+
+            Console.WriteLine("Votre moyenne est de {0}", dblMoyenneAt);
+            Console.ReadLine();
+
+        }
+    }
+}
